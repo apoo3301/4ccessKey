03d8ec2f63e5ab54ad9acf4adb0c2e42c1cf5f06
paracetamol <140186933+apoo3301@users.noreply.github.com>
2024-09-12 19:39:48 +0200

    Delete client directory

--- client/migrations/0001_sour_kitty_pryde.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "publicKey" text;
--- client/src/auth.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextAuthConfig } from "next-auth";
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import db from "./data";
-import * as schema from "./data/schema";
-import { USER_ROLES } from "./lib/constants";
-import type { AdapterUser } from "@auth/core/adapters";
-import { getTableColumns } from "drizzle-orm";
