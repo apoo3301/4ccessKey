307395351cf94c2d404571dc0fb1bf1f3d96949d
witchbl4des <menzo.smit-adam@epitech.eu>
2024-09-12 19:53:48 +0200

    db connection

---
+++ client/src/data/database.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const DATABASE_TOKEN = process.env.DATA_BASE_TOKEN;
+
+const connect = async () => {
+    const connectionState = mongoose.connection.readyState;
+    
+    if (connectionState === 1) {
+        console.log('Database is already connected');
+        return;
+    }
+
+    if (connectionState === 2) {
+        console.log('Connecting to database...');
+        return;
+    }
+
+    try {
+        await mongoose.connect(DATABASE_TOKEN!, {
+            dbName: '0x',
+            bufferCommands: true,
+        });
+        console.log('Database connected');
+    } catch (error : any) {
+        console.log('Database connection error', error);
+        throw new Error('Database connection error');
+    }
+}
+
+export default connect;
