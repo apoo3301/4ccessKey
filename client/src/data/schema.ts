ad128de5799915b066a4cf0efb3db93d42bb9d8c
witchbl4des <menzo.smit-adam@epitech.eu>
2024-09-13 14:10:13 +0200

    role sys

--- client/src/data/schema.ts
+++ client/src/data/schema.ts
@@ -3,10 +3,13 @@ import generateUUIDv7 from "@/security/uuid";
 import { sql, SQL } from "drizzle-orm";
 import { time } from "console";
 
+export const roleEnum = pgEnum("role", ["user", "admin"]);
+
 export const users = pgTable("users", {
     id: text("id").primaryKey().$defaultFn(() => generateUUIDv7()),
     username: text("username"),
     email: text("email").notNull(),
     password: text("password"),
-    createdAt: timestamp("created_at").notNull().defaultNow()
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    role: roleEnum("role").notNull().default("user"),
 })
