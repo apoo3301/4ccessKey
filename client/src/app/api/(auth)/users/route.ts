1fa30cdf4edd9a7a1dc3bee90ff2db2dbdf00652
witchbl4des <menzo.smit-adam@epitech.eu>
2024-09-13 09:59:20 +0200

    usercreate

--- client/src/app/api/(auth)/users/route.ts
+++ client/src/app/api/(auth)/users/route.ts
@@ -11,3 +11,16 @@ export const GET = async () => {
         return new NextResponse(JSON.stringify(error), { status: 500 });
     }
 }
+
+export const POST = async (request : Request) => {
+    try {
+        const body = await request.json();
+        await connect();
+        const userUser = new User(body);
+        await userUser.save();
+
+        return new NextResponse(JSON.stringify({ message: 'User created', user:userUser }), { status: 200 });
+    } catch (error: any) {
+        return new NextResponse(JSON.stringify(error), { status: 500 });
+    }
+}
