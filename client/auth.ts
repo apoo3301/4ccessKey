e22908762e93aaa647bcae1c2087813f2910f426
witchbl4des <menzo.smit-adam@epitech.eu>
2024-09-15 11:59:45 +0200

    auth config, adapter, settings page protected route

--- client/auth.ts
+++ client/auth.ts
@@ -1,9 +1,13 @@
-import Github from "next-auth/providers/github";
+import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/data/database";
 
 export const {
     handlers: { GET, POST },
     auth,
 } = NextAuth({
-    providers: [Github]
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
 })
